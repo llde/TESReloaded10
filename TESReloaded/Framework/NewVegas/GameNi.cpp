b5680abef5d604d79d718664dc2a1365f7029a66
llde <lorenzofersteam@live.it>
2022-09-09 17:51:39 +0200

    Add IsPartitionEnabled for New Vegas

---
+++ TESReloaded/Framework/NewVegas/GameNi.cpp
@@ -0,0 +1,14 @@
+#include "GameNi.h"
+
+bool NiSkinInstance::IsPartitionEnabled(UInt32 partitionIndex) {
+    void* VFT = *(void**)this;
+    if (VFT == Pointers::VirtualTables::BSDismemberSkinInstance){
+        BSDismemberSkinInstance* t = (BSDismemberSkinInstance*) this;
+        if (t->IsRenderable == 0) return false;
+        if (t->partitions && t->partitionNumber < partitionIndex){
+            DismemberPartition p = t->partitions[partitionIndex];
+            if (!p.Enabled) return false;
+        }
+    }
+    return true;
+} 
