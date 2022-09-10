5e0d640d8cd2ea20aab91570b276153d236d8dc4
mchaptel <m.chaptel@gmail.com>
2022-09-11 01:44:48 +0200

    only copy more recent files during deploy

--- build.bat
+++ build.bat
@@ -4,19 +4,19 @@
 "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\msbuild" -target:NewVegasReloaded /property:Configuration=Release /property:Platform=x86 -m
 
 @REM Copy dll & pdb
-xcopy "NewVegasReloaded\Release\NewVegasReloaded.dll" "build\NewVegasReloaded\nvse\Plugins\" /y
-xcopy "NewVegasReloaded\Release\NewVegasReloaded.pdb" "build\NewVegasReloaded\nvse\Plugins\" /y
+xcopy "NewVegasReloaded\Release\NewVegasReloaded.dll" "build\NewVegasReloaded\nvse\Plugins\" /y /d
+xcopy "NewVegasReloaded\Release\NewVegasReloaded.pdb" "build\NewVegasReloaded\nvse\Plugins\" /y /d
 
 @REM Copy config
-xcopy "Extra\NewVegasReloaded.dll.config" "build\NewVegasReloaded\nvse\Plugins\" /y
+xcopy "Extra\NewVegasReloaded.dll.config" "build\NewVegasReloaded\nvse\Plugins\" /y /d
 
 @REM Copy Shaders
-xcopy "Extra\Shaders\NewVegasReloaded\." "build\NewVegasReloaded\Shaders\NewVegasReloaded\" /y /s
+xcopy "Extra\Shaders\NewVegasReloaded\." "build\NewVegasReloaded\Shaders\NewVegasReloaded\" /y /d /s
 
 @REM Copy to deploy if arg was passed
 if "%~1" NEQ "" (
-    echo deploying to "%"
-    xcopy "build\NewVegasReloaded\." "%~1" /y /s /f
+    echo deploying to "%~1"
+    xcopy "build\NewVegasReloaded\." "%~1" /y /d /s
 ) else (
     echo "No deploy stage."
 )
