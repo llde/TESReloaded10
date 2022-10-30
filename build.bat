8040eece5ee34da6d5512c8cb5c793f46ff31848
mchaptel <m.chaptel@gmail.com>
2022-10-30 20:09:10 +0700

    change build.bat to use robocopy in order to preserve files timestamps

--- build.bat
+++ build.bat
@@ -9,7 +9,7 @@ if "%PROJECT%" NEQ "NewVegasReloaded" (
     if "%PROJECT%" NEQ "OblivionReloaded" (
         echo "Project %PROJECT% invalid. Use 'NewVegasReloaded' or 'OblivionReloaded'."
         PAUSE
-        exit %ERRORLEVEL%
+        exit /b %ERRORLEVEL%
     )
 )
 
@@ -19,26 +19,42 @@ msbuild -target:%PROJECT% /property:Configuration=Release /property:Platform=x86
 if %ERRORLEVEL% NEQ 0 (
     echo Build has failed. Deploy aborted.
     PAUSE
-    exit %ERRORLEVEL%
+    exit /b %ERRORLEVEL%
 )
 
+@REM Cleanup in case of previous build
+rmdir "build" /s /q
+
+if "%PROJECT%" == "NewVegasReloaded" (
+    SET "FOLDER=nvse"
+)
+if "%PROJECT%" == "OblivionReloaded" (
+    SET "FOLDER=obse"
+)
 
 @REM Copy dll & pdb
-xcopy "%PROJECT%\Release\%PROJECT%.dll" "build\%PROJECT%\nvse\Plugins\" /y /d
-xcopy "%PROJECT%\Release\%PROJECT%.pdb" "build\%PROJECT%\nvse\Plugins\" /y /d
+xcopy "%PROJECT%\Release\%PROJECT%.dll" "build\%PROJECT%\%FOLDER%\Plugins\" /y /d
+xcopy "%PROJECT%\Release\%PROJECT%.pdb" "build\%PROJECT%\%FOLDER%\Plugins\" /y /d
 
 @REM Copy config
-xcopy "Extra\%PROJECT%.dll.config" "build\%PROJECT%\nvse\Plugins\" /y /d
+xcopy "Extra\%PROJECT%.dll.config" "build\%PROJECT%\%FOLDER%\Plugins\" /y /d
 
 @REM Copy Shaders
-xcopy "Extra\Shaders\%PROJECT%\." "build\%PROJECT%\Shaders\%PROJECT%\" /y /d /s
+robocopy /mir .\Extra\Shaders\%PROJECT%\ .\build\%PROJECT%\Shaders\%PROJECT%\
+
+@REM Copy Textures
+robocopy /mir .\Extra\Textures\ .\build\%PROJECT%\Textures\
 
 @REM Copy to deploy if 2nd arg was passed
 if "%DEPLOY_LOCATION%" NEQ "" (
     echo deploying to "%DEPLOY_LOCATION%"
-    xcopy "build\%PROJECT%\." "%DEPLOY_LOCATION%" /y /d /s
+    robocopy /mir "build\%PROJECT%\." "%DEPLOY_LOCATION%"
 ) else (
     echo "No deploy stage."
 )
 
 PAUSE
+
+@REM handle Robocopy weird error codes
+if (%ERRORLEVEL% LSS 8) exit /b 0
+exit /b %ERRORLEVEL%
