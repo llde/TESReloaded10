df8ca3a7159def0a0edc5dbbc1fdeff200a9d871
MathieuC <m.chaptel@gmail.com>
2022-12-16 22:09:36 +0100

    extract blur, blending and depth/normal reconstruction functions into includes

---
+++ Extra/Shaders/NewVegasReloaded/Effects/Includes/Blur.hlsl
@@ -0,0 +1,60 @@
+static const int cKernelSize = 12;
+static const float BlurWeights[cKernelSize] = 
+{
+	0.057424882f,
+	0.058107773f,
+	0.061460144f,
+	0.071020611f,
+	0.088092873f,
+	0.106530916f,
+	0.106530916f,
+	0.088092873f,
+	0.071020611f,
+	0.061460144f,
+	0.058107773f,
+	0.057424882f
+};
+ 
+static const float2 BlurOffsets[cKernelSize] = 
+{
+	float2(-6.0f * TESR_ReciprocalResolution.x, -6.0f * TESR_ReciprocalResolution.y),
+	float2(-5.0f * TESR_ReciprocalResolution.x, -5.0f * TESR_ReciprocalResolution.y),
+	float2(-4.0f * TESR_ReciprocalResolution.x, -4.0f * TESR_ReciprocalResolution.y),
+	float2(-3.0f * TESR_ReciprocalResolution.x, -3.0f * TESR_ReciprocalResolution.y),
+	float2(-2.0f * TESR_ReciprocalResolution.x, -2.0f * TESR_ReciprocalResolution.y),
+	float2(-1.0f * TESR_ReciprocalResolution.x, -1.0f * TESR_ReciprocalResolution.y),
+	float2( 1.0f * TESR_ReciprocalResolution.x,  1.0f * TESR_ReciprocalResolution.y),
+	float2( 2.0f * TESR_ReciprocalResolution.x,  2.0f * TESR_ReciprocalResolution.y),
+	float2( 3.0f * TESR_ReciprocalResolution.x,  3.0f * TESR_ReciprocalResolution.y),
+	float2( 4.0f * TESR_ReciprocalResolution.x,  4.0f * TESR_ReciprocalResolution.y),
+	float2( 5.0f * TESR_ReciprocalResolution.x,  5.0f * TESR_ReciprocalResolution.y),
+	float2( 6.0f * TESR_ReciprocalResolution.x,  6.0f * TESR_ReciprocalResolution.y)
+};
+
+// perform depth aware 12 taps blur along the direction of the offsetmask
+float4 BlurRChannel(VSOUT IN, uniform float2 OffsetMask, uniform float blurRadius,uniform float depthDrop,uniform float endFade) : COLOR0
+{
+	float WeightSum = 0.114725602f;
+	float4 color1 = tex2D(TESR_RenderedBuffer, IN.UVCoord);
+	color1.r = color1.r * WeightSum;
+
+	// float Depth1 = unpackDepth(ao.gb);
+	float depth1 = readDepth(IN.UVCoord);
+	clip(endFade - depth1);
+
+	int i = 0;
+    for (i = 0; i < cKernelSize; i++)
+    {
+		float2 uvOff = (BlurOffsets[i] * OffsetMask) * blurRadius;
+		float4 color2 = tex2D(TESR_RenderedBuffer, IN.UVCoord + uvOff).r;
+		float depth2 = readDepth(IN.UVCoord + uvOff);
+		// float Depth2 = unpackDepth(Color.gb);
+		float diff = abs(float(depth1 - depth2));
+
+		int useForBlur = (diff <= depthDrop);
+		color1.r += BlurWeights[i] * color2.r * useForBlur;
+		WeightSum += BlurWeights[i] * useForBlur;
+    }
+	color1.r /= WeightSum;
+    return color1;
+}
