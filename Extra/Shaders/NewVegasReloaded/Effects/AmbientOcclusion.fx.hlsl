67e9119f0148b18e17e18f75dc351ab81418aff9
MathieuC <m.chaptel@gmail.com>
2022-12-16 22:09:36 +0100

    extract blur, blending and depth/normal reconstruction functions into includes

--- Extra/Shaders/NewVegasReloaded/Effects/AmbientOcclusion.fx.hlsl
+++ Extra/Shaders/NewVegasReloaded/Effects/AmbientOcclusion.fx.hlsl
@@ -2,13 +2,9 @@
 
 #define viewao 0
 
-float4x4 TESR_ProjectionTransform;
-float4x4 TESR_InvProjectionTransform;
-float4 TESR_ReciprocalResolution;
 float4 TESR_AmbientOcclusionAOData;
 float4 TESR_AmbientOcclusionData;
-float4 TESR_DepthConstants;
-float4 TESR_CameraData;
+float4 TESR_ReciprocalResolution;
 float4 TESR_FogDistance; // x: fog start, y: fog end, z: weather percentage, w: sun glare
 float4 TESR_FogColor;
 
@@ -17,58 +13,16 @@ sampler2D TESR_DepthBuffer : register(s1) = sampler_state { ADDRESSU = CLAMP; AD
 sampler2D TESR_SourceBuffer : register(s2) = sampler_state { ADDRESSU = CLAMP; ADDRESSV = CLAMP; MAGFILTER = LINEAR; MINFILTER = LINEAR; MIPFILTER = LINEAR; };
 sampler2D TESR_NoiseSampler : register(s3) < string ResourceName = "Effects\noise.dds"; > = sampler_state { ADDRESSU = WRAP; ADDRESSV = WRAP; MAGFILTER = NONE; MINFILTER = NONE; MIPFILTER = NONE; };
 
-static const float nearZ = TESR_CameraData.x;
-static const float farZ = TESR_CameraData.y;
-static const float aspectRatio = TESR_CameraData.z;
-static const float fov = TESR_CameraData.w;
-static const float depthRange = farZ - nearZ;
-static const float halfFOV = radians(fov*0.5);
-static const float Q = farZ/(farZ - nearZ);
-
 static const float AOsamples = TESR_AmbientOcclusionAOData.x;
 static const float AOstrength = TESR_AmbientOcclusionAOData.y;
 static const float AOclamp = TESR_AmbientOcclusionAOData.z;
 static const float AOrange = TESR_AmbientOcclusionAOData.w;
 static const float AOangleBias = TESR_AmbientOcclusionData.x;
 static const float AOlumThreshold = TESR_AmbientOcclusionData.y;
-static const float AOblurDrop = TESR_AmbientOcclusionData.z;
-static const float AOblurRadius = TESR_AmbientOcclusionData.w;
-static const float2 texelSize = float2(TESR_ReciprocalResolution.x, TESR_ReciprocalResolution.y);
-static const int cKernelSize = 12;
+static const float blurDrop = TESR_AmbientOcclusionData.z;
+static const float blurRadius = TESR_AmbientOcclusionData.w;
 static const int startFade = 2000;
 static const int endFade = 8000;
-
-static const float BlurWeights[cKernelSize] = 
-{
-	0.057424882f,
-	0.058107773f,
-	0.061460144f,
-	0.071020611f,
-	0.088092873f,
-	0.106530916f,
-	0.106530916f,
-	0.088092873f,
-	0.071020611f,
-	0.061460144f,
-	0.058107773f,
-	0.057424882f
-};
- 
-static const float2 BlurOffsets[cKernelSize] = 
-{
-	float2(-6.0f * TESR_ReciprocalResolution.x, -6.0f * TESR_ReciprocalResolution.y),
-	float2(-5.0f * TESR_ReciprocalResolution.x, -5.0f * TESR_ReciprocalResolution.y),
-	float2(-4.0f * TESR_ReciprocalResolution.x, -4.0f * TESR_ReciprocalResolution.y),
-	float2(-3.0f * TESR_ReciprocalResolution.x, -3.0f * TESR_ReciprocalResolution.y),
-	float2(-2.0f * TESR_ReciprocalResolution.x, -2.0f * TESR_ReciprocalResolution.y),
-	float2(-1.0f * TESR_ReciprocalResolution.x, -1.0f * TESR_ReciprocalResolution.y),
-	float2( 1.0f * TESR_ReciprocalResolution.x,  1.0f * TESR_ReciprocalResolution.y),
-	float2( 2.0f * TESR_ReciprocalResolution.x,  2.0f * TESR_ReciprocalResolution.y),
-	float2( 3.0f * TESR_ReciprocalResolution.x,  3.0f * TESR_ReciprocalResolution.y),
-	float2( 4.0f * TESR_ReciprocalResolution.x,  4.0f * TESR_ReciprocalResolution.y),
-	float2( 5.0f * TESR_ReciprocalResolution.x,  5.0f * TESR_ReciprocalResolution.y),
-	float2( 6.0f * TESR_ReciprocalResolution.x,  6.0f * TESR_ReciprocalResolution.y)
-};
  
 struct VSOUT
 {
@@ -90,97 +44,21 @@ VSOUT FrameVS(VSIN IN)
 	return OUT;
 }
  
+#include "Includes/Depth.hlsl"
+#include "Includes/Blur.hlsl"
 
 // returns a semi random float3 between 0 and 1 based on the given seed.
 // tailored to return a different value for each uv coord of the screen.
 float3 random(float2 seed)
 {
-	return tex2D(TESR_NoiseSampler, (seed/255 + 0.5) / texelSize).xyz;
+	return tex2D(TESR_NoiseSampler, (seed/255 + 0.5) / TESR_ReciprocalResolution.xy).xyz;
 }
 
 // returns a value from 0 to 1 based on the positions of a value between a min/max 
 float invLerp(float from, float to, float value){
   return (value - from) / (to - from);
 }
- 
-float readDepth(in float2 coord : TEXCOORD0)
-{
-	float Depth = tex2D(TESR_DepthBuffer, coord).x;;
-    float ViewZ = (-nearZ *Q) / (Depth - Q);
-	return ViewZ;
-}
 
-
-float3 reconstructPosition(float2 uv)
-{
-	float4 screenpos = float4(uv * 2.0 - 1.0f, tex2D(TESR_DepthBuffer, uv).x, 1.0f);
-	screenpos.y = -screenpos.y;
-	float4 viewpos = mul(screenpos, TESR_InvProjectionTransform);
-	viewpos.xyz /= viewpos.w;
-	return viewpos.xyz;
-}
-
-float3 projectPosition(float3 position){
-	float4 projection = mul(float4 (position, 1.0), TESR_ProjectionTransform);
-	projection.xyz /= projection.w;
-	projection.x = projection.x * 0.5 + 0.5;
-	projection.y = 0.5 - 0.5 * projection.y;
-
-	return projection.xyz;
-}
-
-float3 GetNormal( float2 uv)
-{
-	// improved normal reconstruction algorithm from 
-	// https://gist.github.com/bgolus/a07ed65602c009d5e2f753826e8078a0
-
-	// store coordinates at 1 and 2 pixels from center in all directions
-	float4 rightUv = uv.xyxy + float4(1.0, 0.0, 2.0, 0.0) * texelSize.xyxy; 
-	float4 leftUv = uv.xyxy + float4(-1.0, 0.0, -2.0, 0.0) * texelSize.xyxy; 
-	float4 bottomUv = uv.xyxy + float4(0.0, 1.0, 0.0, 2.0) * texelSize.xyxy; 
-	float4 topUv =uv.xyxy + float4(0.0, -1.0, 0.0, -2.0) * texelSize.xyxy; 
-
-	float depth = readDepth(uv);
-
-	// get depth values at 1 & 2 pixels offsets from current along the horizontal axis
-	half4 H = half4(
-		readDepth(rightUv.xy),
-		readDepth(leftUv.xy),
-		readDepth(rightUv.zw),
-		readDepth(leftUv.zw)
-	);
-
-	// get depth values at 1 & 2 pixels offsets from current along the vertical axis
-	half4 V = half4(
-		readDepth(topUv.xy),
-		readDepth(bottomUv.xy),
-		readDepth(topUv.zw),
-		readDepth(bottomUv.zw)
-	);
-
-	half2 he = abs((2 * H.xy - H.zw) - depth);
-	half2 ve = abs((2 * V.xy - V.zw) - depth);
-
-	// pick horizontal and vertical diff with the smallest depth difference from slopes
-	float3 centerPoint = reconstructPosition(uv);
-	float3 rightPoint = reconstructPosition(rightUv.xy);
-	float3 leftPoint = reconstructPosition(leftUv.xy);
-	float3 topPoint = reconstructPosition(topUv.xy);
-	float3 bottomPoint = reconstructPosition(bottomUv.xy);
-	float3 left = centerPoint - leftPoint;
-	float3 right = rightPoint - centerPoint;
-	float3 down = centerPoint - bottomPoint;
-	float3 up = topPoint - centerPoint;
-
-	half3 hDeriv = he.x < he.y ? left : right;
-	half3 vDeriv = ve.x < ve.y ? down : up;
-
-	// get view space normal from the cross product of the best derivatives
-	// half3 viewNormal = normalize(cross(hDeriv, vDeriv));
-	half3 viewNormal = normalize(cross(vDeriv, hDeriv));
-
-	return viewNormal;
-}
 
 
 float unpackDepth(float2 depth)
@@ -208,7 +86,6 @@ float4 SSAO(VSOUT IN) : COLOR0
 	float2 coord = IN.UVCoord;
 
 	// generate the sampling kernel with random points in a hemisphere
-	// int kernelSize = clamp(int(AOsamples), 0, 8);
 	int kernelSize = 20;
 	float3 kernel[20]; // max supported kernel size is 32 samples
 	float uRadius = abs(AOrange);
@@ -237,7 +114,7 @@ float4 SSAO(VSOUT IN) : COLOR0
 	float occlusion = 0.0;
 	for (i = 0; i < kernelSize; ++i) {
 		// get sample positions around origin:
-		if (dot(normal, kernel[i]) < 0.0) kernel[i] *= -1.0; // if our sample vector goes inside the geometry, we flip it
+		kernel[i] *= dot(normal, kernel[i]) < 0.0 ? -1.0 : 1.0; // if our sample vector goes inside the geometry, we flip it
 		float3 samplePoint = origin + kernel[i] * uRadius;
 		
 		// compare depth of the projected sample with the value from depthbuffer
@@ -250,15 +127,9 @@ float4 SSAO(VSOUT IN) : COLOR0
 		float rangeCheck = distance < uRadius ? 1.0 : 0.0;
 		float influence = (sampleDepth < actualDepth ? 1.0 : 0.0 ) * rangeCheck;
 
-		if (influence){
-			if ( i < kernelSize / 4) {
-				// stronger strength curve in close vectors
-				influence = 1.0 - distance * distance/(uRadius * uRadius);
-			} else {
-				influence = 1.0 - distance /uRadius;
-			}
-			occlusion += influence;
-		}
+		// stronger strength curve in close vectors (replacing an if statement with a lerp)
+		influence *= lerp(1.0 - distance * distance/(uRadius * uRadius), 1.0 - distance /uRadius, i < kernelSize / 4);
+		occlusion += influence;
 	}
 	
 	occlusion = 1.0 - occlusion/kernelSize * AOstrength;
@@ -266,42 +137,10 @@ float4 SSAO(VSOUT IN) : COLOR0
 	float fogColor = Desaturate(TESR_FogColor).x;
 	float darkness = clamp(lerp(occlusion, fogColor, fogCoeff(origin.z)), occlusion, 1.0);
 
-	if (origin.z > startFade){
-		darkness = lerp(darkness, 1.0, saturate(invLerp(0.0, endFade, origin.z)));
-	}
+	darkness = lerp(darkness, 1.0, saturate(invLerp(0.0, endFade, origin.z)));
+	// darkness = lerp(darkness, 1.0, saturate(invLerp(startFade, endFade, origin.z)));
 
 	return float4(darkness, packDepth(origin.z), 1.0);
-}
-
- 
-// perform depth aware 12 taps blur along the direction of the offsetmask
-float4 BlurPS(VSOUT IN, uniform float2 OffsetMask) : COLOR0
-{
-	float WeightSum = 0.114725602f;
-	float4 ao = tex2D(TESR_RenderedBuffer, IN.UVCoord);
-	ao.r = ao.r * WeightSum;
-
-	// float Depth1 = unpackDepth(ao.gb);
-	float Depth1 = readDepth(IN.UVCoord);
-	clip(endFade - Depth1);
-
-	int i = 0;
-    for (i = 0; i < cKernelSize; i++)
-    {
-		float2 uvOff = (BlurOffsets[i] * OffsetMask) * AOblurRadius;
-		float4 Color = tex2D(TESR_RenderedBuffer, IN.UVCoord + uvOff);
-		float Depth2 = readDepth(IN.UVCoord + uvOff);
-		// float Depth2 = unpackDepth(Color.gb);
-		float diff = abs(float(Depth1 - Depth2));
- 
-		if(diff <= AOblurDrop)
-		{
-			ao.r += BlurWeights[i] * Color.r;
-			WeightSum += BlurWeights[i];
-		}
-    }
-	ao.r /= WeightSum;
-    return ao;
 }
 
 
@@ -342,13 +181,13 @@ technique
 	pass
 	{ 
 		VertexShader = compile vs_3_0 FrameVS();
-		PixelShader = compile ps_3_0 BlurPS(float2(1.0f, 0.0f));
+		PixelShader = compile ps_3_0 BlurRChannel(float2(1.0f, 0.0f), blurRadius, blurDrop, endFade);
 	}
 	
 	pass
 	{ 
 		VertexShader = compile vs_3_0 FrameVS();
-		PixelShader = compile ps_3_0 BlurPS(float2(0.0f, 1.0f));
+		PixelShader = compile ps_3_0 BlurRChannel(float2(0.0f, 1.0f), blurRadius, blurDrop, endFade);
 	}
 	
 	pass
---
+++ Extra/Shaders/NewVegasReloaded/Effects/Includes/Depth.hlsl
@@ -0,0 +1,89 @@
+// A collection of functions that allow to query depth of a given pixel and also to reconstruct/project a point from screen to view space
+// requires the shader to get access to the TESR_DepthBuffer sampler before the include.
+
+float4x4 TESR_ProjectionTransform;
+float4x4 TESR_InvProjectionTransform;
+float4 TESR_DepthConstants;
+float4 TESR_CameraData;
+static const float nearZ = TESR_CameraData.x;
+static const float farZ = TESR_CameraData.y;
+static const float Q = farZ/(farZ - nearZ);
+
+float readDepth(in float2 coord : TEXCOORD0)
+{
+	float Depth = tex2D(TESR_DepthBuffer, coord).x;;
+    float ViewZ = (-nearZ *Q) / (Depth - Q);
+	return ViewZ;
+}
+
+float3 reconstructPosition(float2 uv)
+{
+    float4 screenpos = float4(uv * 2.0 - 1.0f, tex2D(TESR_DepthBuffer, uv).x, 1.0f);
+    screenpos.y = -screenpos.y;
+    float4 viewpos = mul(screenpos, TESR_InvProjectionTransform);
+    viewpos.xyz /= viewpos.w;
+    return viewpos.xyz;
+}
+
+float3 projectPosition(float3 position){
+	float4 projection = mul(float4 (position, 1.0), TESR_ProjectionTransform);
+	projection.xyz /= projection.w;
+
+	projection.x = projection.x * 0.5 + 0.5;
+	projection.y = 0.5 - 0.5 * projection.y;
+
+	return projection.xyz;
+}
+
+float3 GetNormal( float2 uv)
+{
+	// improved normal reconstruction algorithm from 
+	// https://gist.github.com/bgolus/a07ed65602c009d5e2f753826e8078a0
+
+	// store coordinates at 1 and 2 pixels from center in all directions
+	float4 rightUv = uv.xyxy + float4(1.0, 0.0, 2.0, 0.0) * TESR_ReciprocalResolution.xyxy; 
+	float4 leftUv = uv.xyxy + float4(-1.0, 0.0, -2.0, 0.0) * TESR_ReciprocalResolution.xyxy; 
+	float4 bottomUv = uv.xyxy + float4(0.0, 1.0, 0.0, 2.0) * TESR_ReciprocalResolution.xyxy; 
+	float4 topUv =uv.xyxy + float4(0.0, -1.0, 0.0, -2.0) * TESR_ReciprocalResolution.xyxy; 
+
+	float depth = readDepth(uv);
+
+	// get depth values at 1 & 2 pixels offsets from current along the horizontal axis
+	float4 H = float4(
+		readDepth(rightUv.xy),
+		readDepth(leftUv.xy),
+		readDepth(rightUv.zw),
+		readDepth(leftUv.zw)
+	);
+
+	// get depth values at 1 & 2 pixels offsets from current along the vertical axis
+	float4 V = float4(
+		readDepth(topUv.xy),
+		readDepth(bottomUv.xy),
+		readDepth(topUv.zw),
+		readDepth(bottomUv.zw)
+	);
+
+	float2 he = abs((2 * H.xy - H.zw) - depth);
+	float2 ve = abs((2 * V.xy - V.zw) - depth);
+
+	// pick horizontal and vertical diff with the smallest depth difference from slopes
+	float3 centerPoint = reconstructPosition(uv);
+	float3 rightPoint = reconstructPosition(rightUv.xy);
+	float3 leftPoint = reconstructPosition(leftUv.xy);
+	float3 topPoint = reconstructPosition(topUv.xy);
+	float3 bottomPoint = reconstructPosition(bottomUv.xy);
+	float3 left = centerPoint - leftPoint;
+	float3 right = rightPoint - centerPoint;
+	float3 down = centerPoint - bottomPoint;
+	float3 up = topPoint - centerPoint;
+
+	float3 hDeriv = he.x < he.y ? left : right;
+	float3 vDeriv = ve.x < ve.y ? down : up;
+
+	// get view space normal from the cross product of the best derivatives
+	// half3 viewNormal = normalize(cross(hDeriv, vDeriv));
+	float3 viewNormal = normalize(cross(vDeriv, hDeriv));
+
+	return viewNormal;
+}
