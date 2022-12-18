aa494000998aa531180b70face8a8c27e4da3e50
MathieuC <m.chaptel@gmail.com>
2022-12-19 00:33:52 +0100

    extract world position reconstruction to depth.hlsl & implement light attenuation for exterior shadows

--- Extra/Shaders/NewVegasReloaded/Effects/Includes/Depth.hlsl
+++ Extra/Shaders/NewVegasReloaded/Effects/Includes/Depth.hlsl
@@ -3,8 +3,13 @@
 
 float4x4 TESR_ProjectionTransform;
 float4x4 TESR_InvProjectionTransform;
+float4x4 TESR_InvViewTransform;
+float4x4 TESR_ViewTransform;
+float4x4 TESR_InvWorldViewProjectionTransform;
 float4 TESR_DepthConstants;
 float4 TESR_CameraData;
+float4 TESR_CameraPosition;
+
 static const float nearZ = TESR_CameraData.x;
 static const float farZ = TESR_CameraData.y;
 static const float Q = farZ/(farZ - nearZ);
@@ -33,6 +38,29 @@ float3 projectPosition(float3 position){
 	projection.y = 0.5 - 0.5 * projection.y;
 
 	return projection.xyz;
+}
+
+
+float3 toWorld(float2 tex)
+{
+	float3 v = float3(TESR_ViewTransform[0][2], TESR_ViewTransform[1][2], TESR_ViewTransform[2][2]);
+	v += (1 / TESR_ProjectionTransform[0][0] * (2 * tex.x - 1)).xxx * float3(TESR_ViewTransform[0][0], TESR_ViewTransform[1][0], TESR_ViewTransform[2][0]);
+	v += (-1 / TESR_ProjectionTransform[1][1] * (2 * tex.y - 1)).xxx * float3(TESR_ViewTransform[0][1], TESR_ViewTransform[1][1], TESR_ViewTransform[2][1]);
+	return v;
+}
+
+float4 reconstructWorldPosition(float2 uv){
+    // float4 screenpos = float4(uv * 2.0 - 1.0f, tex2D(TESR_DepthBuffer, uv).x, 1.0f);
+    // screenpos.y = -screenpos.y;
+    // float4 viewpos = mul(screenpos, TESR_InvWorldViewProjectionTransform);
+    // viewpos.xyz /= viewpos.w;
+    // return viewpos;
+
+
+	float depth = readDepth(uv);
+	float3 camera_vector = toWorld(uv) * depth;
+	float4 world_pos = float4(TESR_CameraPosition.xyz + camera_vector, 1.0f);
+	return world_pos;
 }
 
 float3 GetNormal( float2 uv)
@@ -86,4 +114,11 @@ float3 GetNormal( float2 uv)
 	float3 viewNormal = normalize(cross(vDeriv, hDeriv));
 
 	return viewNormal;
+}
+
+
+float3 GetWorldNormal( float2 uv)
+{
+	float3 normal = GetNormal(uv);
+	return mul(TESR_ViewTransform, normal);
 }
