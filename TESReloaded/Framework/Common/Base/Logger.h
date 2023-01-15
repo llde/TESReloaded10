8f9985b2757391ff6aebd379a4b4589aa450377c
MathieuC <m.chaptel@gmail.com>
2023-01-15 14:18:46 +0100

    add Log::Debug for log prints that can be disabled for release

--- TESReloaded/Framework/Common/Base/Logger.h
+++ TESReloaded/Framework/Common/Base/Logger.h
@@ -4,6 +4,7 @@ class Logger {
 public:
 	static void Initialize(char* FileName);
 	static void Log(char* Message, ...);
+	static void Debug(char* Message, ...);
 	
 //	static char			MessageBuffer[8192];
 	static FILE*		LogFile;
