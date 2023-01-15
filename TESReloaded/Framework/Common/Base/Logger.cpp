f73ae3fd86deb3227a2ec5968e9e1c6294b6d9cd
MathieuC <m.chaptel@gmail.com>
2023-01-15 14:18:46 +0100

    add Log::Debug for log prints that can be disabled for release

--- TESReloaded/Framework/Common/Base/Logger.cpp
+++ TESReloaded/Framework/Common/Base/Logger.cpp
@@ -2,6 +2,8 @@
 //char	Logger::MessageBuffer[8192];
 FILE*	Logger::LogFile;
 
+//#define debugmode
+
 void Logger::Initialize(char* FileName) {
 
 	LogFile = _fsopen(FileName, "w", _SH_DENYWR);
@@ -21,4 +23,18 @@ void Logger::Log(char* Message, ...) {
 		fflush(LogFile);
 	}
 
+}
+
+void Logger::Debug(char* Message, ...) {
+#ifdef debugmode
+	va_list Args;
+	if (LogFile) {
+		va_start(Args, Message);
+		vfprintf_s(LogFile, Message, Args);
+		va_end(Args);
+		//		fputs(MessageBuffer, LogFile);
+		fputc('\n', LogFile);
+		fflush(LogFile);
+	}
+#endif // debugmode
 }
