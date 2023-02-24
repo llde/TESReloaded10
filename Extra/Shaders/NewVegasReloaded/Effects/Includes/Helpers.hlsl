01b18855063daa84e352eec23ef7a1067f109573
MathieuC <m.chaptel@gmail.com>
2023-02-24 22:21:46 +0100

    remove extra ; in helper macros

--- Extra/Shaders/NewVegasReloaded/Effects/Includes/Helpers.hlsl
+++ Extra/Shaders/NewVegasReloaded/Effects/Includes/Helpers.hlsl
@@ -3,8 +3,8 @@
 #define	shade(n, l)         max(dot(n, l), 0)
 #define	shades(n, l)        saturate(dot(n, l))
 #define invlerp(a, b, t)    (t-a)/(b-a)
-#define luma(color)         color.r * 0.3f + color.g * 0.59f +color.b * 0.11f; // extract luma
+#define luma(color)         color.r * 0.3f + color.g * 0.59f +color.b * 0.11f // extract luma
 #define mix(colora, colorb) colora * colorb * 2 // mix two colors without darkening
 #define	weight(v)           dot(v, 1)
 #define	sqr(v)              ((v) * (v))
-#define blendnormals(a, b)  float3(a.xy + b.xy, a.z);
+#define blendnormals(a, b)  float3(a.xy + b.xy, a.z)
