c30b4abd03a9043cf1dd7a4d5f1c8c310526ba50
MathieuC <m.chaptel@gmail.com>
2023-02-26 18:01:58 +0100

    ray attenuation based on proximity to horizon, plug in settings

--- Extra/Shaders/NewVegasReloaded/Effects/GodRays.fx.hlsl
+++ Extra/Shaders/NewVegasReloaded/Effects/GodRays.fx.hlsl
@@ -1,18 +1,15 @@
 // GodRays full screen shader for Oblivion/Skyrim Reloaded
 
-// float4x4 TESR_ViewTransform;
-// float4x4 TESR_ProjectionTransform;
 float4 TESR_ReciprocalResolution;
-float4 TESR_CameraForward;
-float4 TESR_SunDirection;
 float4 TESR_GameTime;
 float4 TESR_SunColor;
 float4 TESR_SunAmount;
-float4 TESR_GodRaysRay;
-float4 TESR_GodRaysRayColor;
-float4 TESR_GodRaysData;
+float4 TESR_GodRaysRay; // x: intensity, y:length, z: density, w: visibility
+float4 TESR_GodRaysRayColor; // x:r, y:g, z:b, w:saturate
+float4 TESR_GodRaysData; // x: passes amount, y: luminance, z:multiplier, w: time enabled
 float4 TESR_DebugVar;
-float4 TESR_ViewSpaceLightDir;
+float4 TESR_ViewSpaceLightDir; // view space light vector
+float4 TESR_SunDirection; // worldspace sun vector
 
 sampler2D TESR_RenderedBuffer : register(s0) = sampler_state { ADDRESSU = CLAMP; ADDRESSV = CLAMP; MAGFILTER = LINEAR; MINFILTER = LINEAR; MIPFILTER = LINEAR; };
 sampler2D TESR_DepthBuffer : register(s1) = sampler_state { ADDRESSU = CLAMP; ADDRESSV = CLAMP; MAGFILTER = LINEAR; MINFILTER = LINEAR; MIPFILTER = LINEAR; };
@@ -22,9 +19,15 @@ sampler2D TESR_SourceBuffer : register(s2) = sampler_state { ADDRESSU = CLAMP; A
 #include "Includes/Depth.hlsl"
 
 static const float raspect = 1.0f / TESR_ReciprocalResolution.z;
-static const float samples = 16;
+static const float samples = 10;
 static const float stepLength = 1/samples;
 static const float scale = 0.5;
+
+static const float lumTreshold = TESR_GodRaysData.y;
+static const float multiplier = TESR_GodRaysData.z;
+static const float glareReduction = TESR_GodRaysRay.z;
+static const float intensity = TESR_GodRaysRay.x;
+static const float stepLengthMult = TESR_GodRaysRay.y;
 
 
 struct VSOUT {
@@ -66,34 +69,31 @@ float4 LightMask(VSOUT IN) : COLOR0 {
 	color += tex2D(TESR_RenderedBuffer, IN.UVCoord + float2(-1, 1) * TESR_ReciprocalResolution.xy).rgb;
 	color += tex2D(TESR_RenderedBuffer, IN.UVCoord + float2(1, -1) * TESR_ReciprocalResolution.xy).rgb;
 	color += tex2D(TESR_RenderedBuffer, IN.UVCoord + float2(1, 1) * TESR_ReciprocalResolution.xy).rgb;
-
 	color /= 4;
 
-	float threshold = TESR_DebugVar.y; // 0.55
+	float threshold = lumTreshold; // 0.55
 	float brightness = luma(color);
+	float bloomScale = intensity; //0.8
 
-	float contribution = max(0.0, brightness - threshold);
-	float nmax = 1;
-	float bloomScale = TESR_DebugVar.z; //0.8
-
-	float bloom = bloomScale * sqr(contribution) / brightness;
+	float bloom = bloomScale * sqr(max(0.0, brightness - threshold)) / brightness;
 
 	return float4(bloom * color * 100, 1.0f);
 }
 
 
-float4 RadialBlur(VSOUT IN, uniform float step, uniform float attenuation) : COLOR0 {
+float4 RadialBlur(VSOUT IN, uniform float step, uniform float fade) : COLOR0 {
 	float2 uv = IN.UVCoord;
 	clip((uv <= scale) - 1);
 	uv /= scale; // restore uv scale to do calculations in [0, 1] space
 
 	// calculate vector from pixel to sun along which we'll sample
 	float2 sunPos = projectPosition(TESR_ViewSpaceLightDir.xyz * farZ).xy;
-	float2 blurDirection = (sunPos.xy - uv) * float2(1.0f, raspect) * TESR_DebugVar.w; // apply aspect ratio correction
-	float distance = length(blurDirection);
-	clip((distance < 1) -1);
 
-	float2 dir = blurDirection/distance;
+	float2 blurDirection = (sunPos.xy - uv) * float2(1.0f, raspect); // apply aspect ratio correction
+	float distance = length(blurDirection);
+	if (distance > 1 || expand(dot(TESR_ViewSpaceLightDir, float4(0, 0, 1, 1))) < 0) return float4(0, 0, 0, 1);
+
+	float2 dir = blurDirection/distance * stepLengthMult;
 	float maxStep = distance/step + 1;
 
 	// sample the light clamped image from the pixel to the sun for the given amount of samples
@@ -102,7 +102,7 @@ float4 RadialBlur(VSOUT IN, uniform float step, uniform float attenuation) : COL
 	float total = 1;
 	for (float i=1; i <= samples; i++){
 		float length = min(step * i, distance); // clamp sampling vector to the distance from the pixel to the sun
-		samplePos = uv + (dir * length / float2(1, raspect)); // apply aspect ratio correctly
+		samplePos = uv + (dir * length / float2(1, raspect)); // apply aspect ratio correction
 		float doStep = (i <= maxStep && samplePos > 0 && samplePos < 1); // check if we haven't overshot the sun position or exited the screen
 		
 		color += tex2D(TESR_RenderedBuffer, samplePos * scale) * doStep;
@@ -110,17 +110,17 @@ float4 RadialBlur(VSOUT IN, uniform float step, uniform float attenuation) : COL
 	}
 	color /= total;
 
+	// attentuate intensity with distance from sun to fade the edges and reduce sunglare only on second pass
+	float heightAttenuation = max(0.3, (1 - dot(TESR_SunDirection.xyz, float3(0, 0, 1))));
+	float glareAttenuation = max(0.2, pow(saturate(distance), glareReduction));
+	float attenuation = pow (saturate(1 - distance), 2) * glareAttenuation * heightAttenuation;
+	color = lerp(color, color * attenuation * 2, fade);
+
+	// return attenuation.xxxx;
+
 	return float4(color.rgb, 1);
 }
 
-
-float3 BlendSoftLight(float3 a, float3 b)
-{
-	float3 c = 2.0f * a * b * (1.0f + a * (1.0f - b));
-	float3 a_sqrt = sqrt(a);
-	float3 d = (a + b * (a_sqrt - a)) * 2.0f - a_sqrt;
-	return (b < 0.5f) ? c : d;
-}
 
 float4 Combine(VSOUT IN) : COLOR0
 {
@@ -128,9 +128,7 @@ float4 Combine(VSOUT IN) : COLOR0
 	float4 color = tex2D(TESR_SourceBuffer, IN.UVCoord);
 	float2 uv = IN.UVCoord * scale;
 	float4 rays = tex2D(TESR_RenderedBuffer, uv);
-
-	rays *= TESR_SunColor;
-
+	rays.rgb *=  multiplier * lerp(TESR_SunColor.rgb, TESR_GodRaysRayColor.rgb, TESR_GodRaysRayColor.w);
 
 	return float4(color.rgb + rays.rgb, 1.0f);
 }
@@ -152,13 +150,19 @@ technique
 	pass
 	{
 		VertexShader = compile vs_3_0 FrameVS();
-		PixelShader = compile ps_3_0 RadialBlur(stepLength, 1); 
+		PixelShader = compile ps_3_0 RadialBlur(stepLength, 0); 
 	}
 
 	pass
 	{
 		VertexShader = compile vs_3_0 FrameVS();
-		PixelShader = compile ps_3_0 RadialBlur(stepLength * stepLength, 1); 
+		PixelShader = compile ps_3_0 RadialBlur(stepLength * stepLength, 0); 
+	}
+
+	pass
+	{
+		VertexShader = compile vs_3_0 FrameVS();
+		PixelShader = compile ps_3_0 RadialBlur(stepLength * stepLength * stepLength, 1); 
 	}
 
 	pass
