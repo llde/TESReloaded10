be7e2240a247ff20e7cac3f44f9eaa77bef1325b
MathieuC <m.chaptel@gmail.com>
2023-03-09 22:50:39 +0100

    cleanup

--- Extra/Shaders/NewVegasReloaded/Effects/Includes/Helpers.hlsl
+++ Extra/Shaders/NewVegasReloaded/Effects/Includes/Helpers.hlsl
@@ -3,7 +3,7 @@
 #define	shade(n, l)         max(dot(n, l), 0)
 #define	shades(n, l)        saturate(dot(n, l))
 #define invlerp(a, b, t)    (t-a)/(b-a)
-#define luma(color)         color.r * 0.3f + color.g * 0.59f +color.b * 0.11f // extract luma
+#define luma(color)         dot(color, float3(0.3f, 0.59f, 0.11f)) // extract luma
 #define mix(colora, colorb) colora * colorb * 2 // mix two colors without darkening
 #define	weight(v)           dot(v, 1)
 #define	sqr(v)              ((v) * (v))
