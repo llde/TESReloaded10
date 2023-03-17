ecaa2276b8a14a83299d0d0c08445fb19604a8c5
MathieuC <m.chaptel@gmail.com>
2023-03-17 23:07:47 +0100

    add some helper functions

--- Extra/Shaders/NewVegasReloaded/Effects/Includes/Helpers.hlsl
+++ Extra/Shaders/NewVegasReloaded/Effects/Includes/Helpers.hlsl
@@ -2,8 +2,10 @@
 #define	compress(v)         (((v) * 0.5) + 0.5)  // from -1/1 to 0/1 
 #define	shade(n, l)         max(dot(n, l), 0)
 #define	shades(n, l)        saturate(dot(n, l))
-#define invlerp(a, b, t)    (t-a)/(b-a)
-#define luma(color)         dot(color, float3(0.3f, 0.59f, 0.11f)) // extract luma
+#define invlerp(a, b, t)    ((t-a)/(b-a))
+#define invlerps(a, b, t)    saturate((t-a)/(b-a))
+#define luma(color)         dot(color, float3(0.2126f, 0.7152f, 0.0722f)) // extract luma (BT.709)
+// #define luma(color)         dot(color, float3(0.3f, 0.59f, 0.11f)) // extract luma (BT.601)
 #define mix(colora, colorb) colora * colorb * 2 // mix two colors without darkening
 #define	weight(v)           dot(v, 1)
 #define	sqr(v)              ((v) * (v))
